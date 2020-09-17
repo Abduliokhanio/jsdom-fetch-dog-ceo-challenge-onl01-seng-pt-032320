2f67883ebcef51b1fe3387ac145ace357eaebf94
Abdul Khan <akhan17@outlook.com>
2020-09-17 15:30:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,3 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
      .then(response => response.json())
      .then(data => data.message.map(k => { addImages(k)}))
  }
+
+ function addImages(image) {
+    let imgContainer = document.querySelector("#dog-image-container")
+    let img = document.createElement("img")
+    img.src = image
+    imgContainer.appendChild(img)
+}
