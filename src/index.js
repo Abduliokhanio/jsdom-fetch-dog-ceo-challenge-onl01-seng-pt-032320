17a257ef0f795b2b1ad8aed806cccf849dff2b03
Abdul Khan <akhan17@outlook.com>
2020-09-17 15:29:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@
  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
  function fetchImages() {
      fetch(imgUrl)
-     .then(response => response.json())     
-     .then(data => data.message.map(k => { addImages(k)}))  
+     .then(response => response.json())
+     .then(data => data.message.map(k => { addImages(k)}))
  }
